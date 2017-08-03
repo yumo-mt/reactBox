c1ee008dab191c9a6831d03deb38c0e37f7408ea
Manster <mansterhaihai@sina.com>
2017-08-03 14:53:32 +0800

    perf

--- src/app/entry.js
+++ src/app/entry.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './app';
+import Perf from 'react-addons-perf';
 
+
+
+import App from './app';
+Perf.start()
 const render2 = Component => {
   render(
     <AppContainer>
@@ -16,4 +20,14 @@ render2(App);
 
 if (module.hot) {
   module.hot.accept('./app', () => { render2(App) });
-}
+};
+Perf.stop();
+var measure = Perf.getLastMeasurements();
+// 打印总时间
+Perf.printInclusive(measure);
+// 打印独占时间（不包括组件挂载时间）
+Perf.printExclusive(measure);
+// 打印浪费的时间（最有用的函数，例如render 了但是DOM没有变化）
+Perf.printWasted(measure);
+//操作真实dom的情况
+Perf.printOperations(measure);
